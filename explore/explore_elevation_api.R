bd1e3bdbff0295035231576db8da6b47aa9e665f
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2025-04-24 10:04:29 +0300

    tests with local data & rstudioapi::askForSecret prompt for API key

--- explore/explore_elevation_api.R
+++ explore/explore_elevation_api.R
@@ -1,6 +1,25 @@
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
 library(sf)
+library(tmaptools)
 
-ves <- read_sf(here::here("data/ves2025.kml"))
+# Local data only! TODO: add accessible example data
+
+# gpx_file <- here::here("data/ves2025.gpx")
+kml_file <- here::here("data/ves2025.kml")
+
+# ves_gpx <- tmaptools::read_GPX(gpx_file, remove.empty.layers = TRUE)[["route_points"]]
+
+sf::st_layers(kml_file)
+ves <- st_read(kml_file, layer = "Points") |>
+  transmute(id = Name, geometry)
+
+
+APIKEY <- rstudioapi::askForSecret("Google Maps Platform API key")
 
 # Create API reader for call
 # https://maps.googleapis.com/maps/api/elevation/json?locations=LAT,LONG&key=YOURAPIKEY
+
+
